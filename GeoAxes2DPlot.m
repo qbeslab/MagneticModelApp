8b754b39075e55a211176d6c6ace141f9f3f9d4d
Jeffrey Gill <jeffrey.p.gill@gmail.com>
2023-08-07 14:53:58 -0400

    Move common elements of GeoAxes2DPlot and GeoGlobe3DPlot into superclass

--- GeoAxes2DPlot.m
+++ GeoAxes2DPlot.m
@@ -1,19 +1,9 @@
-classdef GeoAxes2DPlot < handle
+classdef GeoAxes2DPlot < AbstractMagneticMap
     %GEOAXES2DPLOT Class for managing a 2D GeographicAxes plot of the magnetic environment
     % Required add-ons (use MATLAB's Add-On Explorer to install):
     %   - Mapping Toolbox
     % Optional add-ons (use MATLAB's Add-On Explorer to install):
     %   - MATLAB Basemap Data - colorterrain (install for offline use)
-    
-    properties
-        magmodel MagneticModel
-        agent Agent
-        ax matlab.graphics.axis.GeographicAxes
-        trajectory matlab.graphics.chart.primitive.Line
-        start matlab.graphics.chart.primitive.Line
-        goal matlab.graphics.chart.primitive.Line
-        position matlab.graphics.chart.primitive.Line
-    end
     
     methods
         function obj = GeoAxes2DPlot(magmodel, agent, parent)
@@ -21,7 +11,11 @@ classdef GeoAxes2DPlot < handle
             if nargin == 2
                 parent = gcf;
             end
-            obj.magmodel = magmodel;
+            obj@AbstractMagneticMap(magmodel, agent, parent);
+        end
+
+        function InitializeAxes(obj, parent)
+            %INITIALIZEAXES Initialize GeographicAxes
             obj.ax = geoaxes(parent, Basemap="colorterrain");
             obj.ax.InnerPosition = obj.ax.OuterPosition;
             geolimits(obj.ax, [-70, 70], [-180, 180]);  % aspect ratio constraints often override this
@@ -32,122 +26,15 @@ classdef GeoAxes2DPlot < handle
                 % do nothing in normal figure
             end
             hold(obj.ax);
-
-            obj.AddContourPlots;
-            obj.AddAgent(agent);
         end
 
-        function AddContourPlots(obj)
-            %ADDCONTOURPLOTS Add magnetic property contours to map
-
-            % interpm_maxdiff = 0.1;  % degrees (or nan to skip contour interpolation)
-            interpm_maxdiff = nan;  % degrees (or nan to skip contour interpolation)
-
-            % add contours
-            for param = ["I_INCL", "F_TOTAL"]
-                contour_table = obj.magmodel.contour_tables.(param);
-                nContours = max(contour_table.Group);
-
-                % plot each contour level one at a time
-                for i = 1:nContours
-                    gidx = contour_table.Group == i;
-                    level = unique(contour_table.Level(gidx));
-
-                    % set line width
-                    linewidth = 0.5;  % default
-                    switch param
-                        case "I_INCL"
-                            if mod(level, 30) == 0
-                                linewidth = 3;  % multiples of 30 degrees
-                            else
-                                if mod(level, 10) == 0
-                                    linewidth = 2;  % multiples of 10 degrees
-                                end
-                            end
-                        case "F_TOTAL"
-                            if mod(level, 10000) == 0
-                                linewidth = 2;  % multiples of 10000 nT
-                            end
-                    end
-
-                    % set color
-                    color = 'k';  % default
-                    switch param
-                        case "I_INCL"
-                            % color = "#02B187";  % green from Taylor (2018)
-                            color = "#EEEEEE";  % light gray
-                            if level == 0
-                                color = '#FF8080';  % light red, magnetic equator
-                            end
-                        case "F_TOTAL"
-                            % color = "#8212B4";  % purple from Taylor (2018)
-                            color = "#444444";  % dark gray
-                    end
-
-                    % get contour line coordinates
-                    contour_lat = contour_table.Y(gidx);
-                    contour_lon = contour_table.X(gidx);
-                    if ~isnan(interpm_maxdiff)
-                        % interpolate points on the contour line if necessary
-                        [contour_lat, contour_lon] = interpm(contour_lat, contour_lon, interpm_maxdiff, 'gc');
-                    end
-
-                    % prepare data tooltips and other metadata
-                    %   note: lines can be accessed later via tags, e.g., findobj(obj.ax, "Tag", "I_INCL = 0")
-                    tag = [char(string(param)), ' = ', char(string(level))];
-                    datatipvalues = level*ones(size(contour_lat));
-                    datatiplabel = param.replace('_', '\_');  % default
-                    datatipformat = 'auto';  % default
-                    switch param
-                        case "I_INCL"
-                            datatiplabel = "Inclination";
-                            datatipformat = 'degrees';
-                        case "F_TOTAL"
-                            datatiplabel = "Intensity";
-                            datatipformat = '%g nT';
-                    end
-
-                    % plot contour line
-                    line = geoplot(obj.ax, ...
-                        contour_lat, contour_lon, ...
-                        '-', LineWidth=linewidth, Color=color, ...
-                        Tag=tag, UserData=datatipvalues ...
-                        );
-                    line.DataTipTemplate.DataTipRows(end+1) = dataTipTextRow(datatiplabel, 'UserData', datatipformat);
-                end
-            end
+        function line = AddLine(obj, lat, lon, linespec, varargin)
+            %ADDLINE Plot a line or markers on the map
+            line = geoplot(obj.ax, lat, lon, linespec, varargin{:});
         end
 
-        function AddAgent(obj, agent)
-            %ADDAGENT Add trajectory of an agent to maps
-
-            obj.agent = agent;
-            
-            linewidth = 2;
-            color = "magenta";
-            
-            % plot trajectory
-            %   coords are wrapped here since otherwise the 2D plot would
-            %   draw the trajectory off screen when longitude is outside
-            %   [-180, 180] (3D plot does not need this correction)
-            [new_lat, new_lon] = obj.Wrap2DTrajectoryAroundLon180(obj.agent.trajectory_lat, obj.agent.trajectory_lon);
-            obj.trajectory = geoplot(obj.ax, ...
-                new_lat, new_lon, ...
-                '-', LineWidth=linewidth, Color=color, Marker='none', MarkerSize=2);
-
-            % plot markers for agent start, goal, and current position
-            obj.start = geoplot(obj.ax, obj.agent.start_lat, obj.agent.start_lon, 'bo', MarkerSize=8, LineWidth=2);
-            obj.goal = geoplot(obj.ax, obj.agent.goal_lat, obj.agent.goal_lon, 'go', MarkerSize=8, LineWidth=2);
-            obj.position = geoplot(obj.ax, obj.agent.trajectory_lat(end), wrapTo180(obj.agent.trajectory_lon(end)), 'mo', MarkerSize=8, LineWidth=2);
-
-            % update plots when agent changes
-            addlistener(obj.agent, 'StartChanged', @obj.UpdateAgentStart);
-            addlistener(obj.agent, 'GoalChanged', @obj.UpdateAgentGoal);
-            addlistener(obj.agent, 'TrajectoryChanged', @obj.UpdateAgentTrajectory);
-        end
-
-        function [new_lat, new_lon] = Wrap2DTrajectoryAroundLon180(~, lat, lon)
-            %WRAP2DTRAJECTORYAROUNDLON180 Wrap longitude values to [-180, 180] and insert NaNs to prevent plotting jumps
+        function [new_lat, new_lon] = CleanLatLon(~, lat, lon)
+            %CLEANLATLON Wrap longitude values to [-180, 180] and insert NaNs to prevent plotting jumps
             new_lon = wrapTo180(lon);
             new_lat = lat;
             jumps = find(abs(diff(new_lon)) > 350);
@@ -157,50 +44,6 @@ classdef GeoAxes2DPlot < handle
                     new_lat = [new_lat(1:i); nan; new_lat(i+1:end)];
                 end
             end
-        end
-
-        function ToggleAgentTrajectoryMarkers(obj)
-            %TOGGLEAGENTTRAJECTORYMARKERS Toggle the display of markers for every trajectory step
-            if obj.trajectory.Marker == "none"
-                obj.trajectory.Marker = 'o';
-            else
-                obj.trajectory.Marker = 'none';
-            end
-
-            drawnow;  % force figure to update immediately
-        end
-
-        function UpdateAgentStart(obj, ~, ~)
-            %UPDATEAGENTSTART Update marker for agent start
-
-            obj.start.LatitudeData = obj.agent.start_lat;
-            obj.start.LongitudeData = obj.agent.start_lon;
-
-            drawnow;  % force figure to update immediately
-        end
-
-        function UpdateAgentGoal(obj, ~, ~)
-            %UPDATEAGENTGOAL Update marker for agent goal
-
-            obj.goal.LatitudeData = obj.agent.goal_lat;
-            obj.goal.LongitudeData = obj.agent.goal_lon;
-
-            drawnow;  % force figure to update immediately
-        end
-
-        function UpdateAgentTrajectory(obj, ~, ~)
-            %UPDATEAGENTTRAJECTORY Update plots/markers of agent trajectory and current position
-
-            % coords are wrapped here since otherwise the 2D plot would
-            % draw the trajectory off screen when longitude is outside
-            % [-180, 180] (3D plot does not need this correction)
-            [new_lat, new_lon] = obj.Wrap2DTrajectoryAroundLon180(obj.agent.trajectory_lat, obj.agent.trajectory_lon);
-            obj.trajectory.LatitudeData = new_lat;
-            obj.trajectory.LongitudeData = new_lon;
-            obj.position.LatitudeData = new_lat(end);
-            obj.position.LongitudeData = new_lon(end);
-
-            drawnow;  % force figure to update immediately
         end
     end
 end
--- GeoGlobe3DPlot.m
+++ GeoGlobe3DPlot.m
@@ -1,19 +1,9 @@
-classdef GeoGlobe3DPlot < handle
+classdef GeoGlobe3DPlot < AbstractMagneticMap
     %GEOGLOBE3DPLOT Class for managing a 3D GeographicGlobe plot of the magnetic environment
     % Required add-ons (use MATLAB's Add-On Explorer to install):
     %   - Mapping Toolbox
     % Optional add-ons (use MATLAB's Add-On Explorer to install):
     %   - MATLAB Basemap Data - colorterrain (install for offline use)
-    
-    properties
-        magmodel MagneticModel
-        agent Agent
-        ax globe.graphics.GeographicGlobe
-        trajectory map.graphics.primitive.Line
-        start map.graphics.primitive.Line
-        goal map.graphics.primitive.Line
-        position map.graphics.primitive.Line
-    end
     
     methods
         function obj = GeoGlobe3DPlot(magmodel, agent, parent)
@@ -21,130 +11,26 @@ classdef GeoGlobe3DPlot < handle
             if nargin == 2
                 parent = uifigure;
             end
-            obj.magmodel = magmodel;
+            obj@AbstractMagneticMap(magmodel, agent, parent);
+        end
+
+        function InitializeAxes(obj, parent)
+            %INITIALIZEAXES Initialize GeographicGlobe
             obj.ax = geoglobe(parent, Basemap="colorterrain", Terrain="none");  % Terrain="none" flattens terrain so it does not occlude contours and trajectories
             campos(obj.ax, 0, 0, 1e7);  % manually setting a camera position right away bypasses camera animation, speeding up initial plotting
             % obj.ax.Position = [0 0 1 1];
             hold(obj.ax);
-
-            obj.AddContourPlots;
-            obj.AddAgent(agent);
         end
 
-        function AddContourPlots(obj)
-            %ADDCONTOURPLOTS Add magnetic property contours to map
-
-            % interpm_maxdiff = 0.1;  % degrees (or nan to skip contour interpolation)
-            interpm_maxdiff = nan;  % degrees (or nan to skip contour interpolation)
-
-            % add contours
-            for param = ["I_INCL", "F_TOTAL"]
-                contour_table = obj.magmodel.contour_tables.(param);
-                nContours = max(contour_table.Group);
-
-                % plot each contour level one at a time
-                for i = 1:nContours
-                    gidx = contour_table.Group == i;
-                    level = unique(contour_table.Level(gidx));
-
-                    % set line width
-                    linewidth = 0.5;  % default
-                    switch param
-                        case "I_INCL"
-                            if mod(level, 30) == 0
-                                linewidth = 3;  % multiples of 30 degrees
-                            else
-                                if mod(level, 10) == 0
-                                    linewidth = 2;  % multiples of 10 degrees
-                                end
-                            end
-                        case "F_TOTAL"
-                            if mod(level, 10000) == 0
-                                linewidth = 2;  % multiples of 10000 nT
-                            end
-                    end
-
-                    % set color
-                    color = 'k';  % default
-                    switch param
-                        case "I_INCL"
-                            % color = "#02B187";  % green from Taylor (2018)
-                            color = "#EEEEEE";  % light gray
-                            if level == 0
-                                color = '#FF8080';  % light red, magnetic equator
-                            end
-                        case "F_TOTAL"
-                            % color = "#8212B4";  % purple from Taylor (2018)
-                            color = "#444444";  % dark gray
-                    end
-
-                    % get contour line coordinates
-                    contour_lat = contour_table.Y(gidx);
-                    contour_lon = contour_table.X(gidx);
-                    if ~isnan(interpm_maxdiff)
-                        % interpolate points on the contour line if necessary
-                        [contour_lat, contour_lon] = interpm(contour_lat, contour_lon, interpm_maxdiff, 'gc');
-                    end
-
-                    % prepare data tooltips and other metadata
-                    %   note: lines can be accessed later via tags, e.g., findobj(obj.ax, "Tag", "I_INCL = 0")
-                    tag = [char(string(param)), ' = ', char(string(level))];
-                    datatipvalues = level*ones(size(contour_lat));
-                    % datatiplabel = param.replace('_', '\_');  % default
-                    % datatipformat = 'auto';  % default
-                    % switch param
-                    %     case "I_INCL"
-                    %         datatiplabel = "Inclination";
-                    %         datatipformat = 'degrees';
-                    %     case "F_TOTAL"
-                    %         datatiplabel = "Intensity";
-                    %         datatipformat = '%g nT';
-                    % end
-
-                    % plot contour line
-                    %   note: 3D globe does not use data tooltips
-                    geoplot3(obj.ax, ...
-                        contour_lat, contour_lon, [], ...
-                        '-', LineWidth=linewidth, Color=color, ...
-                        Tag=tag, UserData=datatipvalues ...
-                        );
-                end
-            end
+        function line = AddLine(obj, lat, lon, linespec, varargin)
+            %ADDLINE Plot a line or markers on the map
+            line = geoplot3(obj.ax, lat, lon, [], linespec, varargin{:});
         end
 
-        function AddAgent(obj, agent)
-            %ADDAGENT Add trajectory of an agent to maps
-
-            obj.agent = agent;
-            
-            linewidth = 2;
-            color = "magenta";
-            
-            % plot trajectory
-            obj.trajectory = geoplot3(obj.ax, ...
-                obj.agent.trajectory_lat, obj.agent.trajectory_lon, [], ...
-                '-', LineWidth=linewidth, Color=color, Marker='none', MarkerSize=2);
-
-            % plot markers for agent start, goal, and current position
-            obj.start = geoplot3(obj.ax, obj.agent.start_lat, obj.agent.start_lon, [], 'bo', MarkerSize=8, LineWidth=2);
-            obj.goal = geoplot3(obj.ax, obj.agent.goal_lat, obj.agent.goal_lon, [], 'go', MarkerSize=8, LineWidth=2);
-            obj.position = geoplot3(obj.ax, obj.agent.trajectory_lat(end), obj.agent.trajectory_lon(end), [], 'mo', MarkerSize=8, LineWidth=2);
-
-            % update plots when agent changes
-            addlistener(obj.agent, 'StartChanged', @obj.UpdateAgentStart);
-            addlistener(obj.agent, 'GoalChanged', @obj.UpdateAgentGoal);
-            addlistener(obj.agent, 'TrajectoryChanged', @obj.UpdateAgentTrajectory);
-        end
-
-        function ToggleAgentTrajectoryMarkers(obj)
-            %TOGGLEAGENTTRAJECTORYMARKERS Toggle the display of markers for every trajectory step
-            if obj.trajectory.Marker == "none"
-                obj.trajectory.Marker = 'o';
-            else
-                obj.trajectory.Marker = 'none';
-            end
-
-            drawnow;  % force figure to update immediately
+        function [new_lat, new_lon] = CleanLatLon(~, lat, lon)
+            %CLEANLATLON No cleaning necessary on 3D plots, so do nothing
+            new_lat = lat;
+            new_lon = lon;
         end
 
         function Lock3DCamera(obj)
@@ -199,35 +85,6 @@ classdef GeoGlobe3DPlot < handle
             obj.Lock3DCamera;
             [lat, lon, ~] = campos(obj.ax);
             obj.agent.SetGoal(lat, lon);
-        end
-
-        function UpdateAgentStart(obj, ~, ~)
-            %UPDATEAGENTSTART Update marker for agent start
-
-            obj.start.LatitudeData = obj.agent.start_lat;
-            obj.start.LongitudeData = obj.agent.start_lon;
-
-            drawnow;  % force figure to update immediately
-        end
-
-        function UpdateAgentGoal(obj, ~, ~)
-            %UPDATEAGENTGOAL Update marker for agent goal
-
-            obj.goal.LatitudeData = obj.agent.goal_lat;
-            obj.goal.LongitudeData = obj.agent.goal_lon;
-
-            drawnow;  % force figure to update immediately
-        end
-
-        function UpdateAgentTrajectory(obj, ~, ~)
-            %UPDATEAGENTTRAJECTORY Update plots/markers of agent trajectory and current position
-
-            obj.trajectory.LatitudeData = obj.agent.trajectory_lat;
-            obj.trajectory.LongitudeData = obj.agent.trajectory_lon;
-            obj.position.LatitudeData = obj.agent.trajectory_lat(end);
-            obj.position.LongitudeData = obj.agent.trajectory_lon(end);
-
-            drawnow;  % force figure to update immediately
         end
     end
 end
