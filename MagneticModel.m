e38ba5b2889351047fef124f2c6bfdbf3bdc3866
Jeffrey Gill <jeffrey.p.gill@gmail.com>
2023-08-11 12:38:33 -0400

    Store only inclination and intensity samples in MagneticModel

--- MagneticModel.m
+++ MagneticModel.m
@@ -28,9 +28,12 @@ classdef MagneticModel < handle
             obj.sample_latitudes = -89.5:0.5:89.5;
             obj.sample_longitudes = -180:1:180;
             s = nan(length(obj.sample_latitudes), length(obj.sample_longitudes));
+            % obj.samples = struct( ...
+            %     X_NORTH=s, Y_EAST=s, Z_DOWN=s, ...
+            %     H_HORIZ=s, D_DECL=s, I_INCL=s, ...
+            %     F_TOTAL=s);
             obj.samples = struct( ...
-                X_NORTH=s, Y_EAST=s, Z_DOWN=s, ...
-                H_HORIZ=s, D_DECL=s, I_INCL=s, ...
+                I_INCL=s, ...
                 F_TOTAL=s);
 
             obj.contour_levels = struct( ...
@@ -58,12 +61,13 @@ classdef MagneticModel < handle
             %POPULATESAMPLES Collect samples of magnetic field properties at all coords
             for i = 1:length(obj.sample_latitudes)
                 for j = 1:length(obj.sample_longitudes)
-                    [X, Y, Z, H, D, I, F] = obj.EvaluateModel(obj.sample_latitudes(i), obj.sample_longitudes(j));
-                    obj.samples.X_NORTH(i, j) = X;
-                    obj.samples.Y_EAST (i, j) = Y;
-                    obj.samples.Z_DOWN (i, j) = Z;
-                    obj.samples.H_HORIZ(i, j) = H;
-                    obj.samples.D_DECL (i, j) = D;
+                    % [X, Y, Z, H, D, I, F] = obj.EvaluateModel(obj.sample_latitudes(i), obj.sample_longitudes(j));
+                    [~, ~, ~, ~, ~, I, F] = obj.EvaluateModel(obj.sample_latitudes(i), obj.sample_longitudes(j));
+                    % obj.samples.X_NORTH(i, j) = X;
+                    % obj.samples.Y_EAST (i, j) = Y;
+                    % obj.samples.Z_DOWN (i, j) = Z;
+                    % obj.samples.H_HORIZ(i, j) = H;
+                    % obj.samples.D_DECL (i, j) = D;
                     obj.samples.I_INCL (i, j) = I;
                     obj.samples.F_TOTAL(i, j) = F;
                 end
