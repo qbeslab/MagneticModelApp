e7590e1717420ae9e163e3d5151bb5ac47244716
Jeffrey Gill <jeffrey.p.gill@gmail.com>
2023-09-05 13:19:47 -0400

    Add more commented-out code for checking analytical form of eigenvalues

--- Axesm3DMagneticMap.m
+++ Axesm3DMagneticMap.m
@@ -234,6 +234,12 @@ classdef Axesm3DMagneticMap < AbstractMagneticMap
                     jacobian = -obj.agent.A * [obj.dlonF(i, j), obj.dlatF(i, j);
                                                obj.dlonI(i, j), obj.dlatI(i, j)];
                     ev = eig(jacobian);
+                    evreal = real(ev);
+                    evimag = imag(ev);
+                    tol = 1e-6;
+                    is_unstable = evreal(1) > tol || evreal(2) > tol;
+                    is_neutrally_stable = abs(evreal(1)) < tol || abs(evreal(2)) < tol;
+                    has_rotation = abs(evimag(1)) > tol || abs(evimag(2)) > tol;
 
                     % % check analytical form of jacobian eigenvalues
                     % a = obj.agent.A(1, 1);
@@ -248,19 +254,35 @@ classdef Axesm3DMagneticMap < AbstractMagneticMap
                     % detJ = (a * d - b * c) * (dFdx * dIdy - dFdy * dIdx);
                     % ev2 = [(trJ - sqrt(trJ^2 - 4 * detJ)) / 2;
                     %        (trJ + sqrt(trJ^2 - 4 * detJ)) / 2];
+                    % is_unstable2 = trJ > tol || detJ < -tol;
+                    % is_neutrally_stable2 = abs(detJ) < tol;
+                    % has_rotation2 = trJ^2 < 4 * detJ;
                     % if norm(sort(ev) - sort(ev2)) > 1e-10
                     %     disp("significant deviation found:");
                     %     disp([i, j]);
                     %     disp([sort(ev), sort(ev2)]);
                     %     disp(norm(sort(ev) - sort(ev2)));
                     % end
+                    % if is_unstable ~= is_unstable2
+                    %     disp("disagreement about stability found:");
+                    %     disp([i, j]);
+                    %     disp([sort(ev), sort(ev2)]);
+                    %     disp([is_unstable, is_unstable2]);
+                    %     disp([trJ, detJ]);
+                    % end
+                    % if is_neutrally_stable ~= is_neutrally_stable2
+                    %     disp("disagreement about neutral stability found:");
+                    %     disp([i, j]);
+                    %     disp([sort(ev), sort(ev2)]);
+                    %     disp([is_neutrally_stable, is_neutrally_stable2]);
+                    % end
+                    % if has_rotation ~= has_rotation2
+                    %     disp("disagreement about rotation found:");
+                    %     disp([i, j]);
+                    %     disp([sort(ev), sort(ev2)]);
+                    %     disp([has_rotation, has_rotation2]);
+                    % end
 
-                    evreal = real(ev);
-                    evimag = imag(ev);
-                    tol = 1e-6;
-                    is_unstable = evreal(1) > tol || evreal(2) > tol;
-                    is_neutrally_stable = abs(evreal(1)) < tol || abs(evreal(2)) < tol;
-                    has_rotation = abs(evimag(1)) > tol || abs(evimag(2)) > tol;
                     if is_unstable
                         % at least one eigenvalue real-part is positive: unstable (repelling)
                         if has_rotation
